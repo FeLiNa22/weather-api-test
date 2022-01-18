5dc7a0d00691bb374d7d50c340e73ff04f0bfc0f
Raul <raulpatel97@gmail.com>
2022-01-18 01:24:03 +0000

    removed unecessary file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json()); 
 
-app.get('/', (req, res) => {
+app.get('/predict', (req, res) => {
+    res.send('Well done!');
+})
+
+app.post('/update', (req, res) => {
     res.send('Well done!');
 })
 
